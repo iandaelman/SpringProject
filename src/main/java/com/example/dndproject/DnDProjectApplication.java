29653a9bee02c424bd15329099bf7d1b3fd2a4be
Ian Daelman <ian.daelman@student.hogent.be>
2023-02-28 18:14:36 +0100

    initial commit

---
+++ src/main/java/com/example/dndproject/DnDProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dndproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnDProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnDProjectApplication.class, args);
+    }
+
+}
