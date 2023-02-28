77d390d2332c6d83f87785ca74989fdd195f703d
Ian Daelman <ian.daelman@student.hogent.be>
2023-02-28 19:51:14 +0100

    User entity

---
+++ src/main/java/com/example/dndproject/domein/DomeinController.java
@@ -0,0 +1,7 @@
+package com.example.dndproject.domein;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DomeinController {
+}
---
+++ src/main/java/com/example/dndproject/persistentie/User.java
@@ -0,0 +1,33 @@
+package com.example.dndproject.persistentie;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    protected User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
